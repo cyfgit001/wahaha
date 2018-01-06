f0fc0604ea7b8dec6f225e28c8a84116e0ef006b
Administrator <Administrator@MS-20170829GEKZ.lan>
2018-01-06 09:55:48 +0800

    000

--- src/aaaa/aaa.java
+++ src/aaaa/aaa.java
@@ -6,50 +6,7 @@ public class aaa {
 	System.out.println("000000000000");
 	System.out.println("nihao ");
 	System.out.println("bbbbbb");
-	System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");System.out.println("bbbbbb");
+
 	  
 }
 
