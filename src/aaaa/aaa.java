c1287e375947ad37124f09dd1c73143868f88c53
MS-20170829GEKZ\Administrator <cc694421919@163.com>
2018-01-06 21:27:13 +0800

    0000

--- src/aaaa/aaa.java
+++ src/aaaa/aaa.java
@@ -4,14 +4,7 @@ public class aaa {
 
   public static void main(String[] args) {
 
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");System.out.println("bbbbbb");
+	 
 	  System.out.println("===================");
 	  System.out.println("===================");
 	  System.out.println("===================");
