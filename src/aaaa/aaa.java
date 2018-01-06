b7b0c7b0e567cbe15b346cb630cd3aee7a017a5a
MS-20170829GEKZ\Administrator <cc694421919@163.com>
2018-01-06 11:50:17 +0800

    vvv

--- src/aaaa/aaa.java
+++ src/aaaa/aaa.java
@@ -3,16 +3,7 @@ package aaaa;
 public class aaa {
   
   public static void main(String[] args) {
-	System.out.println("000000000000");
-	System.out.println("nihao ");
-	System.out.println("bbbbbb");
-	System.out.println("bbbbbb");
-	  System.out.println("bbbbbb");
-	System.out.println("bbbbbb");System.out.println("bbbbbb");
-	System.out.println("bbbbbb");System.out.println("bbbbbb");
-	System.out.println("bbbbbb");System.out.println("bbbbbb");
-	System.out.println("bbbbbb");System.out.println("bbbbbb");
-	  
-}
+     System.out.println("7777777777");
+  }
 
 }
