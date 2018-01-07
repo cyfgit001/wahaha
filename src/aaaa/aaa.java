a5d5eee38093f2a5842bbc9e46949579247075f3
MS-20170829GEKZ\Administrator <cc694421919@163.com>
2018-01-07 15:05:21 +0800

    干活大范甘迪发光飞碟

--- src/aaaa/aaa.java
+++ src/aaaa/aaa.java
@@ -4,22 +4,7 @@ public class aaa {
 
   public static void main(String[] args) {
 
-	 
-	  System.out.println("===================");
-	  System.out.println("===================");
-	  System.out.println("===================");
-	  System.out.println("===================");
-	  System.out.println("===================");
-	  System.out.println("===================");
-	  System.out.println("===================");
-	  System.out.println("===================");
-	  System.out.println("===================");
-	  System.out.println("===================");
-	  System.out.println("===================");
-	  System.out.println("===================");
-	  System.out.println("===================");
-	  System.out.println("===================");
-	  System.out.println("===================");
+   System.out.println("只有这么句话");
 
 }
 
